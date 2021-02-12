07c8b6c286473add754428864d2df9f75d651088
BHAVYA KOGANTI <bhavya.koganti@ibm.com>
2021-02-12 11:30:39 +0530

    Modified Abc.java and created xyz.java

--- xyz.java
+++ xyz.java
@@ -1,10 +1,10 @@
-public class Abc
+public class xyz
 {
 	public static void main(String args[])
 	{
 		for(int i=0;i<10;i++)
 		{
-			System.out.println("Hello there");
+			System.out.println("Hello everybody");
 		}
 	}
 }
